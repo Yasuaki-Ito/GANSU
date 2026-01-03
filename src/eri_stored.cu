139eeb9ce1245a838592892cb11ee406219640a8
Yasuaki-Ito <65140854+Yasuaki-Ito@users.noreply.github.com>
2026-01-03 18:46:49 +0900

    Update eri_stored.cu

--- src/eri_stored.cu
+++ src/eri_stored.cu
@@ -1002,10 +1002,10 @@ __device__ __forceinline__ real_t antisym_eri(const real_t* __restrict__ eri_mo,
                                     const int num_basis,
                                     const int p, const int q, const int r, const int s)
 {
-    assert(p >= 0 && p < num_basis);
-    assert(q >= 0 && q < num_basis);
-    assert(r >= 0 && r < num_basis);
-    assert(s >= 0 && s < num_basis);
+    assert(p >= 0 && p < num_basis*2);
+    assert(q >= 0 && q < num_basis*2);
+    assert(r >= 0 && r < num_basis*2);
+    assert(s >= 0 && s < num_basis*2);
 
     // <pq||rs> = (pr|qs) - (ps|qr)
     real_t prqs = ((p%2)==(r%2) && ((q%2)==(s%2))) ? eri_mo[idx4_to_1(num_basis, p/2, r/2, q/2, s/2)] : 0.0;
